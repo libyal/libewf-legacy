f2d6c21836fef310d4d2ea82d5cfc9a391f7470b
Joachim Metz <joachim.metz@gmail.com>
2026-06-22 09:30:05 +0200

    Worked on tests and changes for CI tests

---
+++ synctestdata.ps1
@@ -0,0 +1,87 @@
+# Script that synchronizes the local test data
+
+$TestsInputDirectory = "tests\input"
+$TestSet = "public"
+$TestFiles = "ext2.E01 ext2.raw"
+If (-Not (Test-Path "${TestsInputDirectory}\.libewf_legacy"))
+{
+        New-Item -Name "${TestsInputDirectory}\.libewf_legacy" -ItemType "directory" | Out-Null
+	Write-Output "*.[ELels]01" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.libewf_legacy\glob"
+}
+If (-Not (Test-Path "${TestsInputDirectory}\.pyewf_legacy"))
+{
+        New-Item -Name "${TestsInputDirectory}\.pyewf_legacy" -ItemType "directory" | Out-Null
+	Write-Output "*.[ELels]01" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.pyewf_legacy\glob"
+}
+If (-Not (Test-Path "${TestsInputDirectory}\.ewfacquire"))
+{
+        New-Item -Name "${TestsInputDirectory}\.ewfacquire" -ItemType "directory" | Out-Null
+	Write-Output "*.[Rr][Aa][Ww]" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfacquire\glob"
+}
+If (-Not (Test-Path "${TestsInputDirectory}\.ewfacquire_optical"))
+{
+        New-Item -Name "${TestsInputDirectory}\.ewfacquire_optical" -ItemType "directory" | Out-Null
+	Write-Output "*.[Cc][Uu][Ee]" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfacquire_optical\glob"
+}
+If (-Not (Test-Path "${TestsInputDirectory}\.ewfacquirestream"))
+{
+        New-Item -Name "${TestsInputDirectory}\.ewfacquirestream" -ItemType "directory" | Out-Null
+	Write-Output "*.[Rr][Aa][Ww]" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfacquirestream\glob"
+}
+If (-Not (Test-Path "${TestsInputDirectory}\.ewfexport_legacy"))
+{
+        New-Item -Name "${TestsInputDirectory}\.ewfexport_legacy" -ItemType "directory" | Out-Null
+	Write-Output "*.[Ees]01" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfexport_legacy\glob"
+	Write-Output "-fraw" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfexport_legacy\options"
+}
+If (-Not (Test-Path "${TestsInputDirectory}\.ewfexport_logical_legacy"))
+{
+        New-Item -Name "${TestsInputDirectory}\.ewfexport_logical_legacy" -ItemType "directory" | Out-Null
+	Write-Output "*.[Ll]01" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfexport_logical_legacy\glob"
+	Write-Output "-ffiles" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfexport_logical_legacy\options"
+}
+If (-Not (Test-Path "${TestsInputDirectory}\.ewfexport_write"))
+{
+        New-Item -Name "${TestsInputDirectory}\.ewfexport_write" -ItemType "directory" | Out-Null
+	Write-Output "*.[Ees]01" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfexport_write\glob"
+}
+If (-Not (Test-Path "${TestsInputDirectory}\.ewfinfo"))
+{
+        New-Item -Name "${TestsInputDirectory}\.ewfinfo_legacy" -ItemType "directory" | Out-Null
+	Write-Output "*.[ELels]01" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfinfo_legacy\glob"
+}
+If (-Not (Test-Path "${TestsInputDirectory}\.ewfinfo_logical_legacy"))
+{
+        New-Item -Name "${TestsInputDirectory}\.ewfinfo_logical_legacy" -ItemType "directory" | Out-Null
+	Write-Output "*.[Ll]01" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfinfo_logical_legacy\glob"
+	Write-Output "-H" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfinfo_logical_legacy\options"
+}
+If (-Not (Test-Path "${TestsInputDirectory}\.ewfverify_legacy"))
+{
+        New-Item -Name "${TestsInputDirectory}\.ewfverify_legacy" -ItemType "directory" | Out-Null
+	Write-Output "*.[Ees]01" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfverify_legacy\glob"
+	Write-Output "-dmd5" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfverify_legacy\options"
+}
+If (-Not (Test-Path "${TestsInputDirectory}\.ewfverify_logical_legacy"))
+{
+        New-Item -Name "${TestsInputDirectory}\.ewfverify_logical_legacy" -ItemType "directory" | Out-Null
+	Write-Output "*.[Ll]01" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfverify_logical_legacy\glob"
+	Write-Output "-dmd5 -ffiles" | Out-File -Encoding ascii -FilePath "${TestsInputDirectory}\.ewfverify_logical_legacy\options"
+}
+
+If (-Not (Test-Path ${TestsInputDirectory}))
+{
+	New-Item -Name ${TestsInputDirectory} -ItemType "directory" | Out-Null
+}
+If (-Not (Test-Path "${TestsInputDirectory}\${TestSet}"))
+{
+	New-Item -Name "${TestsInputDirectory}\${TestSet}" -ItemType "directory" | Out-Null
+}
+ForEach ($TestFile in ${TestFiles} -split " ")
+{
+	$UrlTestFile = [System.Uri]::EscapeDataString("${TestFile}")
+	$Url = "https://raw.githubusercontent.com/log2timeline/dfvfs/refs/heads/main/test_data/${UrlTestFile}"
+
+	$ProgressPreference = 'SilentlyContinue'
+	Invoke-WebRequest -Uri ${Url} -OutFile "${TestsInputDirectory}\${TestSet}\${TestFile}"
+}
---
+++ synctestdata.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+# Script that synchronizes the local test data
+
+TESTS_INPUT_DIRECTORY="tests/input"
+TEST_SET="public"
+TEST_FILES="ext2.E01 ext2.raw"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/.libewf_legacy"
+echo "*.[ELels]01" > "${TESTS_INPUT_DIRECTORY}/.libewf_legacy/glob"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/.pyewf_legacy"
+echo "*.[ELels]01" > "${TESTS_INPUT_DIRECTORY}/.pyewf_legacy/glob"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/.ewfacquire"
+echo "*.[Rr][Aa][Ww]" > "${TESTS_INPUT_DIRECTORY}/.ewfacquire/glob"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/.ewfacquire_optical"
+echo "*.[Cc][Uu][Ee]" > "${TESTS_INPUT_DIRECTORY}/.ewfacquire_optical/glob"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/.ewfacquirestream"
+echo "*.[Rr][Aa][Ww]" > "${TESTS_INPUT_DIRECTORY}/.ewfacquirestream/glob"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/.ewfexport_legacy"
+echo "*.[Ees]01" > "${TESTS_INPUT_DIRECTORY}/.ewfexport_legacy/glob"
+echo "-fraw" > "${TESTS_INPUT_DIRECTORY}/.ewfexport_legacy/options"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/.ewfexport_logical_legacy"
+echo "*.[Ll]01" > "${TESTS_INPUT_DIRECTORY}/.ewfexport_logical_legacy/glob"
+echo "-ffiles" > "${TESTS_INPUT_DIRECTORY}/.ewfexport_logical_legacy/options"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/.ewfexport_write"
+echo "*.[Ees]01" > "${TESTS_INPUT_DIRECTORY}/.ewfexport_write/glob"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/.ewfinfo_legacy"
+echo "*.[ELels]01" > "${TESTS_INPUT_DIRECTORY}/.ewfinfo_legacy/glob"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/.ewfinfo_logical_legacy"
+echo "*.[Ll]01" > "${TESTS_INPUT_DIRECTORY}/.ewfinfo_logical_legacy/glob"
+echo "-H" > "${TESTS_INPUT_DIRECTORY}/.ewfinfo_logical/options"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/.ewfverify_legacy"
+echo "*.[Ees]01" > "${TESTS_INPUT_DIRECTORY}/.ewfverify_legacy/glob"
+echo "-dmd5" > "${TESTS_INPUT_DIRECTORY}/.ewfverify_legacy/options"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/.ewfverify_logical_legacy"
+echo "*.[Ll]01" > "${TESTS_INPUT_DIRECTORY}/.ewfverify_logical_legacy/glob"
+echo "-dmd5 -ffiles" > "${TESTS_INPUT_DIRECTORY}/.ewfverify_logical_legacy/options"
+
+mkdir -p "${TESTS_INPUT_DIRECTORY}/${TEST_SET}"
+
+for TEST_FILE in ${TEST_FILES}
+do
+	URL="https://raw.githubusercontent.com/log2timeline/dfvfs/refs/heads/main/test_data/${TEST_FILE}"
+
+	curl -L -o "${TESTS_INPUT_DIRECTORY}/${TEST_SET}/${TEST_FILE}" ${URL}
+done
